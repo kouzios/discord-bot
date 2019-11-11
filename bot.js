bb05e83abada997316c7e97d3b6863f39e04ad71
Matthew Kouzios <mkouzios@hotmail.com>
2019-11-10 18:01:35 -0600

    Updated to hold simple reply functionality only

--- bot.js
+++ bot.js
@@ -1,9 +1,7 @@
 const Discord = require('discord.js');
-var logger = require('winston');
-var auth = require('./auth.json');
-const googleSpeech = require('@google-cloud/speech')
-
-const googleSpeechClient = new googleSpeech.SpeechClient()
+const logger = require('winston');
+const auth = require('./auth.json');
+const axios = require('axios');
 
 // Configure logger settings
 logger.remove(logger.transports.Console);
@@ -25,62 +23,15 @@ bot.on('message', msg => {
     if(content.startsWith("!")) {
         var args = content.substr(1); //Strip away "!"
         switch(args) {
-            case 'ping':
-                msg.reply("Pong!");
-            break;
-            case 'police':
-                police(msg);
+            case 'behavior':
+                    behavior(msg);
             break;
         }
     }
 });
 
-async function police(msg) {
-    var voiceChannel = msg.member.voiceChannel;
-    if(!voiceChannel){
-        msg.reply("Please join a Voice Channel before issuing the !police command!"); 
-        return
-    }
-    
-    //TODO: On disconnect, disconnect bot
-    voiceChannel.join().then(connection => {
-        // events.on('speaking', (userID, SSRC, speaking) => {
-        //     if (!speaking) {
-        //       return
-        //     }
-        
-        //     console.log(`I'm listening to ${user.username}`)
-        
-        //     // this creates a 16-bit signed PCM, stereo 48KHz stream
-        //     const audioStream = bot.getAudioContext(voiceChannel);
-        //     const requestConfig = {
-        //       encoding: 'LINEAR16',
-        //       sampleRateHertz: 48000,
-        //       languageCode: 'en-US'
-        //     }
-        //     const request = {
-        //       config: requestConfig
-        //     }
-        //     const recognizeStream = googleSpeechClient
-        //       .streamingRecognize(request)
-        //       .on('error', console.error)
-        //       .on('data', response => {
-        //         const transcription = response.results
-        //           .map(result => result.alternatives[0].transcript)
-        //           .join('\n')
-        //           .toLowerCase()
-        //         console.log(`Transcription: ${transcription}`)
-        //       })
-        
-        //     const convertTo1ChannelStream = new ConvertTo1ChannelStream()
-        
-        //     audioStream.pipe(convertTo1ChannelStream).pipe(recognizeStream)
-        
-        //     audioStream.on('end', async () => {
-        //       console.log('audioStream end')
-        //     })
-        //   })
-    })
+async function behavior(msg) {
+   msg.reply("WIP")
 }
 
 
